80690f1e2dc7bdb45aaff2c26b33db95c50e4ca0
Evgeniu603 <72415121+Evgeniu603@users.noreply.github.com>
2021-01-26 10:40:47 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,19 @@
 package test;
-
+import java.util.Scanner;
 import domain.Exercise;
-
-public class TestResult {
-
-    public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
-    }
+public class TestResult 
+{
+	public static void main(String[] args) 
+        {
+		try (Scanner sc = new Scanner(System.in)) 
+                {
+			System.out.println("str: ");
+			String str = sc.nextLine();			
+			System.out.println("shift: ");
+			int shiftKey = sc.nextInt();			
+			String crypt = Exercise.crypt(str, shiftKey);
+			System.out.println("Crypt => " + crypt);
+			System.out.println("Decrypt => " + Exercise.decrypt(crypt, shiftKey));
+		}
+	}
 }
